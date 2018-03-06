05623eb1d21c538e70ab06d5e0af00eb1ff41bb6
wangliang <1991wangliang@gmail.com>
2018-03-06 14:32:59 +0800

    sys_service_provider_id 参数

--- alipay-api-service/src/main/java/com/lorne/alipay/utils/AliPayUtils.java
+++ alipay-api-service/src/main/java/com/lorne/alipay/utils/AliPayUtils.java
@@ -57,6 +57,49 @@ public class AliPayUtils {
 
 
 
+    /**
+     * 当面付 ： 条码支付
+     * @param orderNo  商户订单id
+     * @param payCode  用户支付码
+     * @param title    订单标题
+     * @param storeId  商户id
+     * @param totalAmount 总金额
+     * @return  支付结果
+     */
+    public static String barcodePay(AliPayConfig payConfig, String orderNo ,String authToken, String  payCode , String title , String storeId , String totalAmount,String sys_service_provider_id) {
+        AlipayClient alipayClient = new DefaultAlipayClient(
+                AliPayConfig.URL,
+                payConfig.getAppId(),
+                payConfig.getAppPrivateKey(),
+                AliPayConfig.FORMAT,
+                AliPayConfig.CHARSET,
+                payConfig.getAlipayPublicKey(),
+                payConfig.getSignType());
+        AlipayTradePayRequest request = new AlipayTradePayRequest(); //创建API对应的request类
+        request.setBizContent("{" +
+                "    \"out_trade_no\":\""+orderNo+"\"," +
+                "    \"scene\":\"bar_code\"," +
+                "    \"auth_code\":\""+payCode+"\"," +
+                "    \"subject\":\""+title+"\"," +
+                "    \"store_id\":\""+storeId+"\"," +
+                "    \"timeout_express\":\"2m\"," +
+                "    \"extend_params\":{" +
+                "    \"sys_service_provider_id\":\""+sys_service_provider_id+"\\" +
+                "    }," +
+                "    \"total_amount\":\""+totalAmount+"\"" +
+                "  }"); //设置业务参数
+        AlipayTradePayResponse response = null; //通过alipayClient调用API，获得对应的response类
+        try {
+            response = alipayClient.execute(request,null,authToken);
+        } catch (AlipayApiException e) {
+            logger.error(e.getMessage());
+            return null;
+        }
+        return response.getBody();
+    }
+
+
+
 
 
     /**
